d051c21703766cd466c5bf1181457a6d4f0930a5
chatran22 <rubenrasi@gmail.com>
2021-08-03 13:25:28 -0300

    Vuelta atrás de STATIC_URL Corrección de link css

--- DjangoG_Project/settings.py
+++ DjangoG_Project/settings.py
@@ -119,8 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/blog/static/'
-# my_app/static/
+STATIC_URL = '/static/'
+# STATIC_URL = '/blog/static/'
 
 STATIC_ROOT = BASE_DIR / 'static'
 # STATIC_ROOT = BASE_DIR.joinpath('static')
