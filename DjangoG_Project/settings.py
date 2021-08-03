883ce50a0523c5fe596a4a10e825179338952d4d
chatran22 <rubenrasi@gmail.com>
2021-08-03 14:46:22 -0300

    MiMetype css update

--- DjangoG_Project/settings.py
+++ DjangoG_Project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import mimetypes
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -123,6 +124,8 @@ STATIC_URL = '/blog/static/'
 
 STATIC_ROOT = BASE_DIR / 'static'
 # STATIC_ROOT = BASE_DIR.joinpath('static')
+
+mimetypes.add_type("text/css", ".css", True)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
